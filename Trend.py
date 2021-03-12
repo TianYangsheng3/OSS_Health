2accdbaa4ff5d3d7d4dba2412b04c8f244459e46
ysTian <220181691@seu.edu.cn>
2021-03-12 16:20:23 +0800

    add some functions

--- Trend.py
+++ Trend.py
@@ -3,13 +3,13 @@ from mpl_toolkits.axes_grid1 import host_subplot
 import matplotlib.pyplot as plt
 from Month_data import Month_all, Month_one, Month_duration,Month_all_duration, process_data
 from cluster import k_means, choose_project
-from MyPCA import pca_1, compute_newx, LinearReg, compute
+from MyPCA import pca_1, compute_newx, LinearReg, compute, LinearFit
 import os
 from sklearn.preprocessing import normalize
 import csv
 import math
 from Entropy import entropy, get_weights
-from scipy import stats
+from scipy import stats, optimize,misc, special
 
 
 ### 观察项目project_id的健康性与各个评价指标的变化趋势
@@ -98,14 +98,21 @@ def trend_indicator(indicator, pca, start, end, have_std):
         if have_std:
             weights_std.append(cur_weights[ind+11]) # 标准差数据
     # print("weights of "+ indicator + ': ', weights)
-    plt.plot(x, weights, label = "mean")
+    plt.plot(x, weights, label = indicator+"_mean")
+    pred_weight_mean = LinearFit(x, weights)
+    plt.plot(x, pred_weight_mean, '--',label = indicator+"_mean_fit")
     if have_std:
-        plt.plot(x, weights_std, label = "std")    # 标准差数据
+        plt.plot(x, weights_std, label = indicator+"_std")    # 标准差数据
+        pred_weight_std = LinearFit(x, weights_std)
+        plt.plot(x, pred_weight_std, '--', label = indicator+"_std_fit")
     plt.xlabel("months")
     plt.ylabel("weights")
-    plt.title(indicator)
+    plt.title("Change Trend of Evaluation Indicator: "+indicator)
     plt.legend()
-    plt.show()
+    # plt.show()
+    filepath = "fig\\Question_4\\"+indicator+".pdf"
+    plt.savefig(filepath, bbox_inches = 'tight')
+    plt.close()
 
 
 ### 比较项目project_id的从start到end月的两种方法健康性
@@ -142,27 +149,30 @@ def health_hist(scores, start, end):
     print("scores shape: ", scores.shape)
     for i in range(end-start):
         fig, ax = plt.subplots()
-        num_bins = 20
+        num_bins = 30
         # weights = np.ones_like(scores[i])/float(len(scores[i]))
         # counts, num_bins = np.histogram(scores[i])
         # n, bins, patches = ax.hist(scores[i], num_bins, density = True, weights=weights)   # 频率直方图
         n, bins, patches = ax.hist(scores[i], num_bins)         # 频数直方图
 
-        ax.set_xlabel('Scores')
-        ax.set_ylabel('Numbers')
-        ax.set_title('health scores of projects of '+ str(start+i) +  '-th month')
-        print(str(start+i) + "-th month n: ", n)
-
+        ax.set_xlabel('Health')
+        ax.set_ylabel('Project Numbers')
+        ax.set_title('Distribution Histogram of Project Health in '+ str(start+i) +  '-th Month')
+        # print(str(start+i) + "-th month n: ", n)
+        center = (bins[:-1] + bins[1:])/2
+        plt.plot(center, n, '--')
         def autolabel(rects):
             for rect in rects:
                 value = rect.get_height()
                 if value>0:
                     ax.text(rect.get_x()+rect.get_y()/2., value+2, '%d' % value)
                     # ax.text(rect.get_x()+rect.get_y()/2., value*1.02, '%s' % float(round(value*(np.diff(bins)[0]),3)))  
-        
         autolabel(patches)
         fig.tight_layout()
-        plt.show()
+        filepath = "fig\\Question_2\\Histogram_"+str(start+i)+".pdf"
+        plt.savefig(filepath, bbox_inches = 'tight')
+        # plt.show()
+        plt.close()
 
 ###  result of t_test
 def t_test_data(projects_valid, data_proc, weights, scalers, pca, start, end):
@@ -190,7 +200,8 @@ def t_test_data(projects_valid, data_proc, weights, scalers, pca, start, end):
                 
 ### 将相关系数写入到文件中
 def correlation(project_id, data_proc, project_valid, scores, start, end, cnt, have_std):
-    filepath = 'fig\\Question_1\\correlation.csv'
+    filepath = 'fig\\correlation.csv'
+    filepath_2 = 'fig\\p_value.csv'
     header_mean = ['project_id', 'correlation', 'forks_mean','contributor_mean','commits_mean','commit_comment_mean',
         'req_opened_mean','req_closed_mean','req_merged_mean','req_other_mean','issue_mean','issue_comment_mean','watchers_mean']
     header_std = ['forks_std','contributor_std','commits_std','commit_comment_std',
@@ -207,7 +218,7 @@ def correlation(project_id, data_proc, project_valid, scores, start, end, cnt, h
     for j in range(end-start):
         scores_y.append(scores[j][0])
 
-    with open(filepath, 'a+') as f:
+    with open(filepath, 'a+', newline="") as f:
         f_csv = csv.writer(f)
         if cnt==0:
             f_csv.writerow(header)
@@ -222,16 +233,170 @@ def correlation(project_id, data_proc, project_valid, scores, start, end, cnt, h
             v.append(v_cur)
             p.append(p_cur)
         f_csv.writerow([project_id, "cor"] + v)
+        # f_csv.writerow([project_id, "p-value"] + p)
+        # return np.array(v), np.array(p)
+    with open(filepath_2, 'a+', newline="") as f:
+        f_csv = csv.writer(f)
+        if cnt==0:
+            f_csv.writerow(header)
+        v = []
+        p = []
+        for cur in range(num):
+            cur_data = []
+            for i in range(end-start):
+                cur_data.append(data_proc[i][ind][cur]) 
+            v_cur, p_cur = stats.pearsonr(scores_y, cur_data)
+            v.append(v_cur)
+            p.append(p_cur)
+        # f_csv.writerow([project_id, "cor"] + v)
         f_csv.writerow([project_id, "p-value"] + p)
+        
+def interval_stats(scores, start, end):
+
+    # def poisson(k, rate, scale):
+    #     return (scale*(rate**k/special.factorial(k))*np.exp(-rate))
+    
+    # def expon(x, rate, scale):
+    #     return (scale*rate*np.exp(-rate*x))    
+    Count = np.zeros_like(np.arange(99))
+    scores = np.array(scores)
+    lengths  = end-start
+    # print("scores shape: ", scores.shape)
+    for i in range(end-start):
+        fig, ax = plt.subplots()
+        dur_bins = np.linspace(0, 1, 100)
+        # counts, num_bins = np.histogram(scores[i])
+        n, bins, patches = ax.hist(scores[i], bins=dur_bins)         # 频数直方图
+        Count = np.array(n) + Count
+        # ppot, pcov = optimize.curve_fit(poisson, center, n)
+        # plt.plot(center, n, 'g--', label = 'Origin')
+        # plt.plot(center, poisson(center, *ppot), 'r--', label = 'Possion')
+        # ppot, pcov = optimize.curve_fit(expon, center, n)
+        # plt.plot(center, expon(center, *ppot), 'r--', label = 'Expon')
+        # print(ppot)
+        fig.tight_layout()
+        plt.legend()
+        plt.close()
+    filepath = "fig\\Question_2\\histogram.csv"
+    headers = [" ", "Numbers", "months"]
+    for i in range(1,100):
+        headers.append(str(i*0.01))
+    Avg = Count/(end-start)
+    with open(filepath, 'w') as f:
+        f_csv = csv.writer(f)
+        f_csv.writerow(headers)
+        f_csv.writerow(["Sum", scores.shape[1], end-start] + list(Count))
+        f_csv.writerow(["Avg", scores.shape[1], end-start] + list(Avg))
+
+ ### 得到每个健康值区间的从start到end月的平均健康值和指标
+def Avg_value_interval(data_proc, scores_all_proj,  projects_valid, selects, start, end, have_std, interval):
+    results = []
+
+    for i in range(len(interval)):
+        print("group_ "+str(i)+": ", len(selects[i]))
+        result_cur = [[] for j in range(end-start)]
+        for pid in selects[i]:
+            ind = projects_valid.index(pid)
+            for k in range(end-start):
+                if len(result_cur[k])==0:
+                    result_cur[k].append(scores_all_proj[k][ind])
+                    for v in data_proc[k][ind]:
+                        result_cur[k].append(v)
+                else:
+                    result_cur[k][0] += scores_all_proj[k][ind]
+                    ccnt = 1
+                    for v in data_proc[k][ind]:
+                        # print("pid: ", data_proc[k][ind])
+                        # print("result_cur: ", result_cur)
+                        result_cur[k][ccnt] += v 
+                        ccnt += 1
+        result_cur = np.array(result_cur)
+        num = len(selects[i])
+        for j in range(end-start):
+            result_cur[j] = result_cur[j]/num
+        results.append(result_cur)
+    results = np.array(results)
+    # print("result shape: ", results.shape)
+    return results   
 
 
+### 画出不同健康值区间的健康值和评价指标对比图
+def compare_interval(results, start, end, have_std, interval):
+    labels = ['health','forks','contributor','commits','commit_comment',
+        'req_opened','req_closed','req_merged','other','issue','issue_comment','watchers']
+    category = ['Low', 'Medium', 'High']
+    rootpath = "fig\\Question_3\\"
+    feature_num = len(labels)
+    if have_std:
+        feature_num = 2*len(labels) - 1
+    x = range(start, end)
+    for i in range(feature_num):
+        value = [[] for k in range(len(interval))]
+        for j in range(end-start):
+            for cate in range(len(interval)):
+                value[cate].append(results[cate][j][i])
+        for p in range(len(interval)):
+            if i>=12:
+                name = labels[i-11]+'_std'
+            else:
+                name = labels[i]
+            plt.plot(x, value[p], label=category[p] +": "+ name)
+        plt.legend()
+        plt.xlabel("month")
+        plt.ylabel("value")
+        #plt.show()
+        if i>=12:
+            fpath = rootpath + labels[i-11]+'_std.pdf'
+        else:
+            fpath = rootpath + labels[i] + '.pdf'
+        plt.savefig(fpath, bbox_inches = 'tight')
+        plt.close()
+
+    # 计算斜率
+    # x = range(start, end)
+    # for i in range(len(interval)):
+    #     print("group_0: ", len(selects[i]))
+    #     cnt0, cnt1, cnt2 = 0, 0, 0
+    #     sum_coef0,sum_coef1,sum_coef2  = 0, 0, 0
+    #     for pid in selects[i]:
+    #         ind = projects_valid.index(pid)
+    #         cur_scores = []
+    #         for cur in range(end-start):
+    #             cur_scores.append(scores_all_proj[cur][ind])
+    #         cur_coef = MyPCA.Coef(x, cur_scores)
+    #         if cur_coef[0][0]>0:
+    #             cnt0 += 1
+    #             sum_coef0 += cur_coef[0][0]
+    #         elif cur_coef[0][0]==0:
+    #             cnt1 += 1
+    #             sum_coef1 += cur_coef[0][0]
+    #         else:
+    #             cnt2 += 1
+    #             sum_coef2 += cur_coef[0][0]
+    #     print("增加、不变、减少的项目个数分别有： ", cnt0, cnt1, cnt2)
+    #     print("平均斜率分别为：", sum_coef0/cnt0, sum_coef1, sum_coef2/cnt2)
+
+
+
+    # return selects
+    # rootpath = 'Question_3\\group_'
+    # for k in range(len(interval)):
+    #     cur_sel = random.sample(selects[k], 5)
+    #     print("选择的项目id：", cur_sel)
+    #     filepath = rootpath + str(k)
+    #     for pid in cur_sel:
+    #         ind = projects_valid.index(pid)
+    #         scores_pca = []
+    #         for cur in range(end-start):
+    #             scores_pca.append([scores_all_proj[cur][ind]])
+    #         Trend.trend(pid, data_proc, projects_valid, scores_pca, start, end, filepath, have_std=have_std)
 
 
 if __name__ == '__main__':
     labels = ['forks','contributor','commits','commit_comment',
         'req_opened','req_closed','req_merged','other','issue','issue_comment','watchers']
     rootpath = os.getcwd() + '\\data\\'
-    start, end = 1, 3
+    start, end = 0, 10
     project_id = 961
     components_num = 10
     have_std = False
@@ -246,15 +411,16 @@ if __name__ == '__main__':
     ### 得到每个月的信息熵方法的权重
     weights = get_weights(data_proc, scalers, start, end, have_std = have_std)
     ### scores_2记录使用信息熵方法得到的健康性
-    scores_2 = entropy(project_id, projects_valid, data_proc, weights, scalers, start, end)
+    # scores_2 = entropy(project_id, projects_valid, data_proc, weights, scalers, start, end)
 
     ### compare 比较两种方法
-    compare(project_id, start, end, scores, scores_2, filepath = 'Question_1')
+    # compare(project_id, start, end, scores, scores_2, filepath = 'Question_1')
 
     ### 
     # trend(project_id, data_proc, projects_valid, scores, start, end, filepath = 'Question_1',have_std = have_std)
     # for indicator in labels:
     #     trend_indicator(indicator, pca, start, end, have_std = have_std)
-    # scores_all_proj = get_scores_projects(data_proc, projects_valid, scalers, pca, start, end)
+    scores_all_proj = get_scores_projects(data_proc, projects_valid, scalers, pca, start, end)
     # health_hist(scores_all_proj, start, end)
+    interval_stats(scores_all_proj, start, end)
     # t_test_data(projects_valid, data_proc, weights, scalers, pca, start, end)
